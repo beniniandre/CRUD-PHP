0a45106a126e3f0cbabc83d62a21bea360932fe4
beniniandre <andreluis_benini@hotmail.com>
2018-03-28 19:14:32 -0300

    Botões

--- CRUD-PHP/CRUD/Inicio/Inicio.php
+++ CRUD-PHP/CRUD/Inicio/Inicio.php
@@ -2,16 +2,27 @@
 <html lang="pt-br">
     <head>
         <link href="../../CSS/Visual.css"  rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative" rel="stylesheet">
         <meta charset="UTF-8">
         <title> CRUD PHP</title>
+        <script type="text/javascript">
+            function AbrePagina(l){
+                location.href = l;
+            }
+        </script>
     </head>
     <body>
-        <h1 class="titulo">CRUD: PHP</h1>
+        <h1 class="titulo">CRUD - PHP</h1>
         <?php
         ?>
+        <section class="login">
+            lalaa            
+            <button class="bt" id="b1" onclick="AbrePagina('../LgIndex.php')">LOGIN</button>
+            <button class="bt" id="b2" onclick="AbrePagina('../Cadastro.php')">NÃO É CADASTRADO? CADASTRE-SE AGORA</button>
+        </section>
         <footer>Desenvolvido por André Benini - Github :  <img  class="im" src="../../Imagens/GH.png" align="absbottom"/> 
             <a class="link1" href="www.github.com/beniniadre">beniniandre</a> | Twitter  :  <img class="im" src="../../Imagens/TW.png" align="absbottom"/> 
             <a class="link2" href="www.twitter.com/beniniandre">@beniniandre
-            </a></footer>
+            </a><br>andreluis_benini@hotmail.com</footer>
     </body>
 </html>
---
+++ CRUD-PHP/CRUD/LgIndex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>CRUD PHP - BEM VINDO</title>
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
